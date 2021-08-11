df6a2565c1836c98afcfc6ec741a92975d4b925a
Hemil78 <zanattahemil@gmail.com>
2021-08-11 17:01:43 +0200

    esercizio mail

---
+++ js/mail.js
@@ -0,0 +1,42 @@
+//Mail
+//chiedi aal'utente la sua mail
+//controlla che sia nella lista di chi può accedere
+//stampa un messaggio appropriato sull'esito del controllo
+
+//creo una lista di mail in array
+var listaMail = ["uno@gmail.com", "due@gmail.com", "tre@gmail.com", "quattro@gmail.com"];
+console.log ( listaMail );
+
+//chiedi aal'utente la sua mail
+var mail = prompt("Inserisci la tua mail");
+
+//se la mail inserita è nella lista
+for ( var i = 0; i < listaMail.length; i++ ) {
+    if ( listaMail[i] == mail ) {
+        document.getElementById("benvenuto").innerHTML = "Benvenuto/a: " + listaMail[i];
+    }else {
+        prompt("Accesso negato!!! Riprova");
+    }
+    console.log(mail);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Gioco dei dadi
+//generare un numero random da 1 a 6, sia per il giocatore , sia per il computer
+//stabilire il vincitore in base a chi fa il punteggio più alto
